7213ab83c77efa655639b05e21fe20d0262426ba
HenryTan0705 <htan0705@gmail.com>
2020-02-25 18:11:44 -0500

    Set up user reducer in entities

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import entitiesReducer from './entities_reducer';
 import sessionsReducer from './sessions_reducer';
 import errorReducer from './errors_reducer';
 
 const rootReducer = combineReducers({
+    entities: entitiesReducer,
     session: sessionsReducer,
     errors: errorReducer
 });
---
+++ frontend/reducers/users_reducer.js
@@ -0,0 +1,17 @@
+import { RECEIVE_CURRENT_USER } from '../actions/session_actions.js';
+import { merge } from 'lodash'
+
+const usersReducer = (state = {}, action) => {
+    Object.freeze(state);
+
+    switch (action.type) {
+        case RECEIVE_CURRENT_USER:
+            let { user } = action;
+            return merge({}, { [user.id]: user });
+        default:
+
+            return state;
+    }
+}
+
+export default usersReducer;
